f1e42c034275bd9d7bb5d2e36c4f35da68c18684
Michael Volk <michael-volk@hotmail.com>
2020-10-11 23:41:53 -0700

    Crude exercise of DS18B20 sensors

--- main/main.c
+++ main/main.c
@@ -1,40 +1,89 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Michael Volk
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/rmt.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "temperatureSensor.h"
 
+typedef struct
+{
+    const char * name;
+    QueueHandle_t queue;
+} TemperatureFeed_t;
+
+uint8_t externalSensorData[2 * sizeof(float)];
+StaticQueue_t externalSensorQueue;
+TemperatureSensor_t externalSensor;
+TemperatureFeed_t externalTemperatureFeed;
+
+uint8_t internalSensorData[2 * sizeof(float)];
+StaticQueue_t internalSensorQueue;
+TemperatureSensor_t internalSensor;
+TemperatureFeed_t internalTemperatureFeed;
+
+void consumeTemperature(void * pvParameters) {
+    TemperatureFeed_t * feed = (TemperatureFeed_t *) pvParameters;
+    QueueHandle_t queue = feed->queue;
+    float reading;
+    while (true) {
+      if (xQueueReceive(queue, &reading, 5000.0 / portTICK_PERIOD_MS) == pdTRUE) {
+          printf("CONSUMED: %.3f C from %s\n", reading, feed->name);
+      } else {
+          printf("FAILED to read from %s queue (empty?)\n", feed->name);
+      }
+    }
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    externalSensor.name = "External sensor";
+    externalSensor.oneWireGPIO = (CONFIG_EXTERNAL_DS18B20_GPIO);
+    externalSensor.tx_channel = RMT_CHANNEL_0;
+    externalSensor.rx_channel = RMT_CHANNEL_1;
+    externalSensor.resolution = DS18B20_RESOLUTION_12_BIT;
+    QueueHandle_t externalTemperatureQueue = xQueueCreateStatic(2, sizeof(float), externalSensorData, &externalSensorQueue);
+    externalSensor.queue = externalTemperatureQueue;
+    xTaskCreate(&senseTemperature, "senseExternalTemperatureTask", 2048, &externalSensor, 5, NULL);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    externalTemperatureFeed.queue = externalTemperatureQueue;
+    externalTemperatureFeed.name = "External sensor";
+    xTaskCreate(&consumeTemperature, "externalTemperatureConsumeTask", 2048, &externalTemperatureFeed, 5, NULL);
 
-    printf("silicon revision %d, ", chip_info.revision);
+    internalSensor.name = "Internal sensor";
+    internalSensor.oneWireGPIO = (CONFIG_INTERNAL_DS18B20_GPIO);
+    internalSensor.tx_channel = RMT_CHANNEL_2;
+    internalSensor.rx_channel = RMT_CHANNEL_3;
+    internalSensor.resolution = DS18B20_RESOLUTION_9_BIT;
+    QueueHandle_t internalTemperatureQueue = xQueueCreateStatic(2, sizeof(float), internalSensorData, &internalSensorQueue);
+    internalSensor.queue = internalTemperatureQueue;
+    xTaskCreate(&senseTemperature, "senseInternalTemperatureTask", 2048, &internalSensor, 5, NULL);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    internalTemperatureFeed.queue = internalTemperatureQueue;
+    internalTemperatureFeed.name = "Internal sensor";
+    xTaskCreate(&consumeTemperature, "internalTemperatureConsumeTask", 2048, &internalTemperatureFeed, 5, NULL);
 }
